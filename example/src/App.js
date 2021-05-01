ca9e47be196ee1ae0436a0e434e798cf1e89e855
Elliot Emmerson <elliotluke.emmerson@gmail.com>
2021-05-01 15:24:08 +0100

    init

---
+++ example/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ReactGol from 'react-gol'
+
+function App() {
+  return (
+    <div className="App">
+      <ReactGol />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,158 @@
+import * as React from 'react';
+import './styles.scss'
+
+interface golProps {
+    cellSize?: number;
+    minFrameTime?: number;
+    fillStyle?: string;
+    motionBlur?: number;
+    initialCoverage?: number;
+}
+
+const gol = (props:golProps) => {
+
+    const canvasRef = React.useRef<HTMLCanvasElement>(document.createElement("canvas"))
+
+    const config = {
+        cellSize: props.cellSize || 12,
+        minFrameTime: props.minFrameTime || 100,
+        fillStyle: props.fillStyle ||'#EEEEEE',
+        motionBlur: props.motionBlur || 50,
+        initialCoverage: props.initialCoverage || .125
+    }
+
+
+    React.useEffect(() => {
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        if (!context) return
+
+        canvas.width = canvas.getBoundingClientRect().width;
+        canvas.height = canvas.getBoundingClientRect().height;
+
+        const gridX = Math.round(canvas.width / config.cellSize)
+        const gridY = Math.round(canvas.height / config.cellSize)
+
+        let grid: Array<Array<number>> = Array(gridX)
+
+        for (let x = 0; x < gridX; x++) {
+            grid[x] = Array(gridY)
+            for (let y = 0; y < gridY; y++) {
+                grid[x][y] = Math.random() < config.initialCoverage ? 1 : 0
+            }
+        }
+
+        let requestId: number;
+        let lastFrameTime = 0;
+
+        context.fillStyle = config.fillStyle
+
+        const render = () => {
+            if (lastFrameTime < Date.now() - config.minFrameTime) {
+
+
+                let tempGrid: Array<Array<number>> = [Array(gridX)]
+
+                for (let x = 0; x < grid.length; x++) {
+                    tempGrid[x] = Array(12)
+                    for (let y = 0; y < grid[x].length; y++) {
+
+                        let self = grid[x][y]
+
+                        const top = getNeighbour(x, y - 1)
+                        const topLeft = getNeighbour(x - 1, y - 1)
+                        const topRight = getNeighbour(x + 1, y - 1)
+                        const left = getNeighbour(x - 1, y)
+                        const right = getNeighbour(x + 1, y)
+                        const bot = getNeighbour(x, y + 1)
+                        const botLeft = getNeighbour(x - 1, y + 1)
+                        const botRight = getNeighbour(x + 1, y + 1)
+                        const neighbours = top + topLeft + topRight + bot + botLeft + botRight + left + right;
+
+                        if (self === 1) {
+                            if (neighbours < 2) {
+                                tempGrid[x][y] = 0
+                            } else if (neighbours > 3) {
+                                tempGrid[x][y] = 0
+                            } else {
+                                tempGrid[x][y] = 1
+                            }
+
+
+                        } else {
+                            if (neighbours == 3) {
+                                tempGrid[x][y] = 1
+                            } else {
+                                tempGrid[x][y] = 0
+                            }
+
+                        }
+                    }
+                }
+
+                grid = tempGrid
+
+                let imageData = context.getImageData(0, 0, canvas.width, canvas.height)
+
+                for (let i = 3; i < imageData.data.length; i += 4) {
+
+                    if (!config.motionBlur) {
+                        imageData.data[i] = 0
+                    } else {
+                        imageData.data[i] -= config.motionBlur
+                    }
+
+                }
+
+                context.putImageData(imageData, 0, 0)
+
+                for (let x = 0; x < grid.length; x++) {
+                    for (let y = 0; y < grid[x].length; y++) {
+                        if (grid[x][y]) {
+                            context.fillRect(x * config.cellSize, y * config.cellSize, config.cellSize, config.cellSize)
+                        }
+                    }
+                }
+
+                lastFrameTime = Date.now()
+            }
+            requestId = requestAnimationFrame(render)
+        }
+
+        const getNeighbour = (x: number, y: number) => {
+            if (x >= gridX) {
+                x = 0
+                return 0
+            }
+            if (x < 0) {
+                x = gridX - 1
+                return 0
+            }
+            if (y >= gridY) {
+                y = 0
+                return 0
+            }
+            if (y < 0) {
+                y = gridY - 1
+                return 0
+            }
+
+            return grid[x][y]
+        }
+
+
+        render();
+
+        return () => {
+            cancelAnimationFrame(requestId);
+        };
+
+
+    }, [])
+
+    return (
+        <canvas ref={canvasRef} className={'canvas'}></canvas>
+    )
+}
+
+export default gol;
